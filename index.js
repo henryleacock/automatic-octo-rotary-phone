a2cd5b8f42938ac1d910665c62f364973b31b22b
Henry Leacock <hl245236@STL2101005MAC.local>
2021-05-20 11:05:40 -0500

    Work from the second session

--- index.js
+++ index.js
@@ -10,9 +10,30 @@ const filter = function (pathname, req) {
 const options = {
     target: 'https://www.famousfootwear.com', // target host
     changeOrigin: true, // needed for virtual hosted sites
+    logLevel: 'info',
+    pathRewrite: async function (path, req) {
+        // if (path.includes('/-/media/themes/tenant/famous-footwear/famous-footwear-common-styles/styles/optimized-min.css')) {
+        if (path.includes('/search')) {
+            console.log('we are here:', path);
+            path = '/this-is-a-test' + path;
+            console.log('after transform:', path);
+        }
+        
+        // const should_add_something = await httpRequestToDecideSomething(path);
+        // if (should_add_something) path += "something";
+        return path;
+      }
 };
 
+// pathRewrite: {'^/-/media/themes/tenant/famous-footwear/famous-footwear-common-styles/styles/optimized-min.css' : 'http://localhost:8000/-/media/themes/tenant/famous-footwear/famous-footwear-common-styles/styles/optimized-min.css'}
+// pathRewrite: {'^/-/media/themes/tenant/famous-footwear/famous-footwear-common-styles/styles/optimized-min.css' : 'http://localhost:8000/optimized-min.css'}
+// http://localhost:3000/http://localhost:8000/optimized-min.css
+// http://localhost:8000
+
 // pathRewrite: {'^/-/media/themes/tenant/famous-footwear/famous-footwear-common-styles/styles/' : '/'}
+// router: {
+//     '^/-/media/themes/tenant/famous-footwear/famous-footwear-common-styles/styles/': 'http://localhost:8000/optimized-min.css'
+// }
  
 // create the proxy (without context)
 const exampleProxy = createProxyMiddleware(options);
@@ -20,6 +41,7 @@ const exampleProxy = createProxyMiddleware(options);
 // mount `exampleProxy` in web server
 const app = express();
 app.use('*', exampleProxy);
+// app.use(express.static('public'));
 app.listen(3000);
 
 
