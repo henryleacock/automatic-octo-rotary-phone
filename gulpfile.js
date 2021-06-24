1b73b68982370bb5a9bd22a1e195833583290571
Henry Leacock <hl245236@STL2101005MAC.attlocal.net>
2021-06-24 15:20:31 -0500

    Update gulp tasks to pull from sitecore project

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,26 @@
 const { series, src, dest } = require('gulp');
 const concat = require('gulp-concat');
 
-// The `build` function is exported so it is public and can be run with the `gulp` command.
-// It can also be used within the `series()` composition.
-function build(cb) {
-    // body omitted
-    cb();
-    console.log('build');
+
+function commonStyles() {
+    return src('../Sitecore_Commerce/Storefront/src/creative-exchange/-/media/Themes/Tenant/Famous Footwear/Famous Footwear Common Styles/styles/common.css')
+        .pipe(concat('optimized-min.css'))
+        .pipe(dest('./-/media/themes/tenant/famous-footwear/famous-footwear-common-styles/styles/'));
 }
 
-function optimize() {
-    return src('./src/*.js')
-        .pipe(concat('all.js'))
-        .pipe(dest('./dist/'));
-    // console.log('optimize');
+function productStyles() {
+    return src('../Sitecore_Commerce/Storefront/src/creative-exchange/-/media/Themes/Tenant/Famous Footwear/Famous Footwear Product Styles/styles/plp-pdp.css')
+        .pipe(concat('optimized-min.css'))
+        .pipe(dest('./-/media/themes/tenant/famous-footwear/famous-footwear-product-styles/styles/'));
 }
 
-// The `clean` function is not exported so it can be considered a private task.
-// It can still be used within the `series()` composition.
-function move(cb) {
-  // body omitted
-  cb();
-  console.log('move');
+function productScripts() {
+    return src('../Sitecore_Commerce/Storefront/src/creative-exchange/-/media/Base Themes/Caleres Commerce Product Components/scripts/*.js')
+        .pipe(concat('optimized-min.js'))
+        .pipe(dest('./-/media/base-themes/caleres-commerce-product-components/scripts/'));
 }
 
-
-
-function listen(cb) {
-    cb();
-    console.log('listen');
-}
-
-exports.build = build;
-exports.optimize = optimize;
-exports.move = move;
-exports.listen = listen;
-exports.runAll = series(build, optimize, move, listen);
+exports.commonStyles = commonStyles;
+exports.productStyles = productStyles;
+exports.productScripts = productScripts;
+exports.optimizeAll = series(commonStyles, productStyles, productScripts);
